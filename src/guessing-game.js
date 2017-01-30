f39b4570974d90048b04daa8bd1c5439683b818c
dissaby <dissaby@gmail.com>
2017-01-30 20:16:16 +0300

    Guessing Game: all tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.min + this.max) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        return this;
     }
 
     greater() {
-
+        this.min = this.middle;
+        return this;
     }
 }
 
